db06c7488117f2e7278443b68880d8c3306f643d
Rob Hilgefort <rob.hilgefort@pwc.com>
2020-01-05 23:40:50 -0700

    actually adding GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,5 +20,11 @@ module.exports = {
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-29584936-5',
+      },
+    },
   ],
 }
